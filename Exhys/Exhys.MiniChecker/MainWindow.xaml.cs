ba7fd56bee46c474c50414781192832fbde35a98
Alexander Ivanov <sashko99@hotmail.com>
2015-07-14 03:33:45 -0700

    Added Message

--- Exhys/Exhys.MiniChecker/MainWindow.xaml.cs
+++ Exhys/Exhys.MiniChecker/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace Exhys.MiniChecker
 {
@@ -10,6 +11,8 @@ namespace Exhys.MiniChecker
         public MainWindow ()
         {
             InitializeComponent();
+            Debug.WriteLine("MainWindow initialized");
+
         }
     }
 }
