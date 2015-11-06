7b54652e9c167f7315461d622bc0f22560a5e292
Ivan Yochev <vani172@gmail.com>
2015-11-07 01:06:06 +0200

    Removed useless projects

---
+++ Exhys/SubmissionRouterService/Services/LocalExecutionCallback.cs
@@ -0,0 +1,18 @@
+﻿using SubmissionRouterService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubmissionRouterService.Dtos;
+
+namespace SubmissionRouterService.Services
+{
+    public class LocalExecutionCallback : IExecutionCallback
+    {
+        public void ExecuteSubmission(Guid submissionProcessId, ExecutionDto execution)
+        {
+            
+        }
+    }
+}
