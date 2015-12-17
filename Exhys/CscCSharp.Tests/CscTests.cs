ca0a2af9e50fb968b3d6473194026d12625d553a
Alexander Ivanov <sashko99@hotmail.com>
2015-12-17 02:39:46 +0200

    Added C# Compiler library

---
+++ Exhys/CscCSharp.Tests/CscTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using Exhys.ExecutionCore;
+using Exhys.ExecutionCore.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CscCSharp.Tests
+{
+    [TestClass]
+    public class CscTests
+    {
+        [TestMethod]
+        public void TestCsHelloWorld ()
+        {
+            Debug.WriteLine("Shit");
+            ICompiler csc = CompilerFactory.Get("c#");// new Csc();
+
+            CompilationResult helloWorldProgram = csc.Compile(csHelloWorld);
+
+            Process testProcess = Process.Start(new ProcessStartInfo(helloWorldProgram.ExecutablePath) { UseShellExecute = false, RedirectStandardOutput = true });
+            testProcess.WaitForExit();
+
+            string programOut = testProcess.StandardOutput.ReadToEnd();
+            if (!programOut.Contains("H")) throw new Exception("Compiler Failed");
+        }
+
+        [TestMethod]
+        public void TestCsHelloWorldWithError ()
+        {
+            ICompiler csc = new Csc();
+            var program = csc.Compile(csHelloWorldWithError);
+
+            if (program.IsSuccessful) throw new Exception("Shoud've yelded an error.");
+        }
+
+        const string csHelloWorld = @"
+        namespace HelloWorld
+        {
+            using System;
+            class Program
+            {
+                static void Main (string[] args)
+                {
+                    Console.WriteLine('H');
+                }
+            }
+        }
+        ";
+
+        const string csHelloWorldWithError = @"
+        #include <iostream>
+        #warning some warning
+        //using namespace std;
+        int main()
+        {
+            cout<<'H'<<endl;
+            return 0;
+        }";
+    }
+}
---
+++ Exhys/CscCSharp/Csc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exhys.ExecutionCore.Contracts;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+namespace CscCSharp
+{
+    [Export(typeof(ICompiler))]
+    public class Csc : ICompiler
+    {
+        public string LanguageAlias { get { return @"c#"; } } 
+
+        public string LanguageName { get { return @"C#"; } }
+
+        public CompilationResult Compile (string sourceCode)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters()
+            {
+                GenerateExecutable = true
+            };
+            var results = provider.CompileAssemblyFromSource(parameters, sourceCode);
+            //if(results.error)
+            StringBuilder outputBuilder = new StringBuilder();
+            foreach(var error in results.Errors)
+            {
+                outputBuilder.AppendLine(error.ToString());
+            }
+            if(results.Errors.HasErrors)
+            {
+                return new CompilationResult(outputBuilder.ToString());
+            }
+            else
+            {
+                return new CompilationResult(results.PathToAssembly, outputBuilder.ToString(), true);
+            }
+        }
+    }
+}
